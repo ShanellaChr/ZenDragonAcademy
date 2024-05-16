56b2e1175cde9faac1362230a0ffa1a09793bf69
Shanella <140229356+ShanellaChr@users.noreply.github.com>
2024-05-16 21:23:52 +0700

    Add files via upload

---
+++ CharacterPage/CharacterPage.js
@@ -0,0 +1,29 @@
+let lastScrollTop = 0;
+let ticking = false;
+
+window.addEventListener("scroll", function() {
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      let currentScroll = window.scrollY || document.documentElement.scrollTop;
+      if (currentScroll > lastScrollTop && currentScroll > 60){
+        document.querySelector(".header").style.top = "-300px";
+      } else {
+        document.querySelector(".header").style.top = "0"; 
+      }
+      lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+      ticking = false;
+    });
+    ticking = true;
+  }
+}, false);
+ 
+
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function(){
+  dropDownMenu.classList.toggle('open')
+  const isOpen = dropDownMenu.classList.contains('open')
+  toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark':'fa-solid fa-bars'
+}
